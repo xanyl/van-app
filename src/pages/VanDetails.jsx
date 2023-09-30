aceb82c50b4d3ceb2eaa566c03e1adc7c7105c39
xanyl <aniltiwari645@gmail.com>
2023-09-30 09:29:46 +0545

    adding vandetails

--- src/pages/VanDetails.jsx
+++ src/pages/VanDetails.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from  "react-router-dom";
 
+export default function VanDetail() {
+  const [vanDetail, setVanDetail] = useState(null);
+  const params = useParams();
 
-export default function VanDetails() {
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data); // Check the data received
+        setVanDetail(data.vans);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
   return (
-    <div>VanDetails</div>
-  )
+    <div className="van-detail-container">
+      {console.log(vanDetail)}
+      {vanDetail ? (
+        <div className="van-detail">
+          <img src={vanDetail.imageUrl} alt={vanDetail.name} />
+          <i className={`van-type ${vanDetail.type} selected`}>{vanDetail.type}</i>
+          <h2>{vanDetail.name}</h2>
+          <p className="van-price">
+            <span>${vanDetail.price}</span>/day
+          </p>
+          <p>{vanDetail.description}</p>
+          <button className="link-button">Rent this van</button>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
 }
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -10,7 +10,7 @@ export default function Vans() {
     fetch("/api/vans")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data); // Check the data received
+        // console.log(data); // Check the data received
         setVans(data.vans);
         setLoading(false);
       })
@@ -27,26 +27,26 @@ export default function Vans() {
   if (error) {
     return <p>Error: {error.message}</p>;
   }
-  const vanElements = vans.map(van => (
-    <div key={van.id} className="van-tile" >
-        <Link to = {`/vans/${van.id}`}>
+  const vanElements = vans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <Link to={`/vans/${van.id}`}>
         <img src={van.imageUrl} />
         <div className="van-info">
-            <h3>{van.name}</h3>
-            <p>${van.price}<span>/day</span></p>
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
         </div>
         <i className={`van-type ${van.type} selected`}>{van.type}</i>
-    
-        </Link>
-        </div>
-))
+      </Link>
+    </div>
+  ));
 
   return (
     <div className="van-list-container">
-        <h1>Explore Our Vans Options</h1>
-            <div className="van-list">
-                {vanElements}
-            </div>
-        </div>
+      <h1>Explore Our Vans Options</h1>
+      <div className="van-list">{vanElements}</div>
+    </div>
   );
 }
